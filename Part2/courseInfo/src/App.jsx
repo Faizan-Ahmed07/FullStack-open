cbd847a66acf74274f6ae6af81dc1bc95a6de711
Blocky-l253098 <l253098@lhr.nu.edu.pk>
2026-03-01 15:45:22 +0500

    Completed exercise 2.1

---
+++ Part2/courseInfo/src/App.jsx
@@ -0,0 +1,57 @@
+
+const Header=({name})=>{
+  console.log(name)
+  return(
+    <h1>{name}</h1>
+  )
+}
+
+const Content = ({name,exercises})=>{
+  console.log(name, exercises,"from content")
+  return(
+    <div>
+      {name} {exercises}
+    </div>
+  )
+}
+
+const Course = ({course})=>{
+  console.log(course, "From course")
+  return(
+    <div>
+      <Header name = {course.name}/>
+      {course.parts.map((item)=>
+        
+        <Content name = {item.name} exercises = {item.exercises} key = {item.id} />
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
