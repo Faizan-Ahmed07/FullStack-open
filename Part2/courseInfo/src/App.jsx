c95af637121bd4b711de86c538866d70efb1b637
Blocky-l253098 <l253098@lhr.nu.edu.pk>
2026-03-02 23:23:55 +0500

    2.4 done

--- Part2/courseInfo/src/App.jsx
+++ Part2/courseInfo/src/App.jsx
@@ -2,7 +2,7 @@
 const Header=({name})=>{
   console.log(name)
   return(
-    <h1>{name}</h1>
+    <h2>{name}</h2>
   )
 }
 
@@ -17,11 +17,6 @@ const Content = ({name,exercises})=>{
 
 const Total = (props)=>{
   const total = props.course.parts.reduce((s, p) => s+p.exercises,0)
-  console.log(total)
-
-  // props.course.parts.map((item)=>{
-  //   total += item.exercises
-  // })
 
   return(
     <h3>Total of {total} exercises</h3>
@@ -34,7 +29,6 @@ const Course = ({course})=>{
     <div>
       <Header name = {course.name}/>
       {course.parts.map((item)=>
-        
         <Content name = {item.name} exercises = {item.exercises} key = {item.id} />
       )}
       <Total course = {course}/>
@@ -43,29 +37,57 @@ const Course = ({course})=>{
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-
-  return <Course course={course} />
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return(
+  <div>
+    <h1>Web Development Curriculum</h1>
+    {/* <Course course={courses} /> */}
+    {courses.map(course=><Course course={course}/> )}
+   </div>
+   )
 }
 
 export default App
